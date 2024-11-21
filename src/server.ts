966a5d940b09f952238a4ca1e31041e0430a4879
aditya <aditya@bucle.dev>
2024-11-21 19:17:39 +0530

    add: add logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
     try {
         const port = Config.PORT;
 
-        app.listen(port, () => console.log(`Listening on Port ${port}`));
+        app.listen(port, () => {
+            logger.error("check error are here");
+            logger.info(`Listening on Port ${port}`);
+        });
     } catch (e) {
         console.log(`error are here ${e}`);
         process.exit(1);
