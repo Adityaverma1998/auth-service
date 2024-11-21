159452014f9e1fa75b45ce78387c64beb5e9c498
aditya <aditya@bucle.dev>
2024-11-21 20:36:42 +0530

    add: setup global error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Welcome auth services");
+app.get("/", async (req, res, next) => {
+    const err = createHttpError(401, "You can't access this router");
+    next(err);
+    // res.send("Welcome auth services");
+});
+
+// global error handling
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const codeStatus = err.statusCode || 500;
+    res.status(codeStatus).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
