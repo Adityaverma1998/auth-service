3478d62d83aff37592fdb99c805f819d7db48e41
aditya <aditya@bucle.dev>
2024-11-27 01:11:48 +0530

    add: add db connection to the server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import app from "./app";
 import { Config } from "./config";
+import { AppDataSource } from "./config/data-source";
 import logger from "./config/logger";
 
-const startServer = () => {
+const startServer = async () => {
     try {
         const port = Config.PORT;
 
+        await AppDataSource.initialize();
+        logger.info("data base connect sucessfully!");
         app.listen(port, () => {
             logger.error("check error are here");
             logger.info(`Listening on Port ${port}`);
